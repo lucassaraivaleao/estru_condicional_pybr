e3cf4a31e603a1f93ee7a8cbd5d3571e3ad93c88
Lucas Saraiva Leão <lucas.saraivaleao@yahoo.com>
2022-02-08 13:00:58 -0300

    Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.

--- exercicio_08.py
+++ exercicio_08.py
@@ -1,5 +1,5 @@
 """
-Exercicio-07:
+Exercicio-08:
 Faça um programa que pergunte o preço de três produtos
 e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 
